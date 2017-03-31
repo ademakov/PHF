6c63ba92c3b30eae6feec7dda0205c9d847f3bf1
Aleksey Demakov <ademakov@gmail.com>
2017-04-01 03:04:15 +0700

    Add rule output op

--- examples/publicsuffix/public-suffix-types.h
+++ examples/publicsuffix/public-suffix-types.h
@@ -2,6 +2,7 @@
 #define PUBLIC_SUFFIX_TYPES_H
 
 #include <cstdint>
+#include <iostream>
 #include <string>
 
 namespace public_suffix {
@@ -13,11 +14,35 @@ enum class Rule {
 	kException,
 };
 
-struct Fnv64 {
+static inline std::ostream &
+operator<<(std::ostream &os, Rule rule)
+{
+	const char *cp = nullptr;
+	switch (rule) {
+	case Rule::kDefault:
+		cp = "Rule::kDefault";
+		break;
+	case Rule::kRegular:
+		cp = "Rule::kRegular";
+		break;
+	case Rule::kWildcard:
+		cp = "Rule::kWildcard";
+		break;
+	case Rule::kException:
+		cp = "Rule::kException";
+		break;
+	}
+	os << cp;
+	return os;
+}
+
+struct Fnv64
+{
 	static constexpr std::uint64_t FNV1_64_INIT = UINT64_C(0xcbf29ce484222325);
 	static constexpr std::uint64_t FNV_64_PRIME = UINT64_C(0x100000001b3);
 
-	std::size_t operator()(const std::string& data, std::uint64_t hval) {
+	std::size_t operator()(const std::string &data, std::uint64_t hval)
+	{
 		auto *bp = reinterpret_cast<const unsigned char *>(data.data());
 		auto *ep = reinterpret_cast<const unsigned char *>(data.data()) + data.size();
 		while (bp < ep) {
